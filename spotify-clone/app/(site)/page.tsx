7e330f9900361a5b73b9e4b83374a220da08e88d
rusia9217 <110519493+unnimm@users.noreply.github.com>
2023-12-04 17:27:14 +0900

    feat: Sidebar Components
    Sidebar Component 구현

---
+++ spotify-clone/app/(site)/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <>
+      <div className="
+      bg-neutral-900
+      rounded-lg
+      h-full
+      w-full
+      overflow-hidden
+      overflow-y-auto"> </div>
+    </>
+  );
+}
