e0ea8f71d61415ed2f11072b1554658a56a8ebc5
rusia9217 <110519493+unnimm@users.noreply.github.com>
2023-12-15 14:56:43 +0900

    feat: getSongs action 추가
    getSongs action 추가

---
+++ spotify-clone/actions/getSongs.ts
@@ -0,0 +1,17 @@
+import { Song } from '../type';
+import { createServerComponentClient } from "@supabase/auth-helper-nextjs"
+import {cookies} from "next/headers";
+
+
+const getSongs = async (): Promise<Song[]> => {
+    const supabase = createServerComponentClient(
+        cookies: cookies
+    );
+
+    const {data, error} = await supabase.from(' songs').select('*').order('created_at', {ascending: false})
+    if (error) {
+        console.log(error)
+    }
+    return (data as any) || [];
+}
+export default getSongs;
