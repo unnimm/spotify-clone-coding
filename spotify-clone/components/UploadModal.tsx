1c162a2c2ceec694c72875315415b81250e7ab24
rusia9217 <110519493+unnimm@users.noreply.github.com>
2023-12-15 23:59:39 +0900

    feat: input 기능 file, image, title, author
     input 기능 file, image, title, author 구현

--- spotify-clone/components/UploadModal.tsx
+++ spotify-clone/components/UploadModal.tsx
@@ -7,6 +7,7 @@ import useUploadModal from "../hooks/useUploadModal";
 
 import Modal from './Modal';
 import Input from './Input';
+import Button from './Button'
 
 
 const UploadModal = () => {
@@ -39,18 +40,54 @@ const UploadModal = () => {
 
   return (
     <Modal
-      title="Add to s ong"
+      title="Add to song"
       description="Upload and mp3 file"
       isOpen={uploadModal.isOpen}
       onChange={onChange}
     >
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-4">
         <Input 
         id="title"
         disabled={isLoading}
         {...register('title', { required: true })}
         placeholder="Song title"
         />
+         <Input 
+        id="author"
+        disabled={isLoading}
+        {...register('author', { required: true })}
+        placeholder="Song author"
+        />
+
+        {/*file 파일 업로드*/}
+        <div>
+          <div className="pb-1">
+            Select a song file
+          </div>
+          <Input 
+        id="song"
+        type="file"
+        disabled={isLoading}
+        accept=".mp3"
+        {...register('song', { required: true })}
+        />
+        </div>
+
+{/*image 파일 업로드*/}
+        <div>
+          <div className="pb-1">
+            Select an image
+          </div>
+          <Input 
+        id="image"
+        type="file"
+        disabled={isLoading}
+        accept="image/*"
+        {...register('image', { required: true })}
+        />
+        </div>
+
+      <Button disabled={isLoading} type="submit">create</Button>
 
       </form>
     </Modal>
